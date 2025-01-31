3f77264f7872e8e2562d599a38a25ee15e646d4e
Tony <anthonytjohnson09@gmail.com>
2025-01-30 21:39:37 -0500

    Merge remote-tracking branch 'origin/main' into main This merges changes from the remote main branch into the local main branch.

--- obj/Debug/net8.0-windows/PC_STAT_TRACKER.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/PC_STAT_TRACKER.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PC_STAT_TRACKER")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1d4058c7ba6771e93713f465a1fc9bfaeaf7b0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("PC_STAT_TRACKER")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PC_STAT_TRACKER")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
